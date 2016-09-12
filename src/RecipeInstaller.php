c6e9375b8ce75e818cb907dbb3580ee8e91346e8
Damian Mooyman <damian@silverstripe.com>
2016-09-13 10:31:34 +1200

    Remove 'silverstripe-recipe' type

--- src/RecipeInstaller.php
+++ src/RecipeInstaller.php
@@ -19,11 +19,10 @@ class RecipeInstaller extends LibraryInstaller {
     public function __construct(
         IOInterface $io,
         Composer $composer,
-        $type = 'silverstripe-recipe',
         Filesystem $filesystem = null,
         BinaryInstaller $binaryInstaller = null
     ) {
-        parent::__construct($io, $composer, $type, $filesystem, $binaryInstaller);
+        parent::__construct($io, $composer, null, $filesystem, $binaryInstaller);
     }
 
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
